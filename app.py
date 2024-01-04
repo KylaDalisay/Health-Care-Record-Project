23046cacecac11714f054a730284064cd06009e1
KylaDalisay <94228516+KylaDalisay@users.noreply.github.com>
2024-01-04 15:11:59 +0000

    add new infos

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
  return render_template('halcyonCenter.html')
+  
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
 
+@app.route('/hospitalHomepage')
+def hospitalhomepage():
+  return render_template('signup.html')
+
+@app.route('/hospitalinformati')
+
+@app.route('/doctorHomepage')
+  def doctorhomepage():
+    return render_template():
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug = True)

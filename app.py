a0d3b7a497d9b12deec276f43d7baceea3012606
KylaDalisay <94228516+KylaDalisay@users.noreply.github.com>
2024-01-04 16:37:19 +0000

    add new list

--- app.py
+++ app.py
@@ -20,7 +20,11 @@ def doctorhomepage():
 
 @app.route('/addPatient')
 def addPatient():
-  return render_template('')
+  return render_template('addPatient.html')
+
+@app.route('/patientlist')
+def patientlist():
+  return render_template('patientlist.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug = True)
